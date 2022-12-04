8ccde5ee9c8e741ae2e9d5be40c2033d417b9257
Erik Kennedy <kennedye@users.noreply.github.com>
2022-12-03 22:22:20 -0800

    should comment out unless being used

--- i75_sample.py
+++ i75_sample.py
@@ -41,23 +41,23 @@ matrix = rgbmatrix.RGBMatrix(
 # )
 
 # matrixportal M4 32x32 or 64x32
-matrix = rgbmatrix.RGBMatrix(
-    width=32,
-    height=32,
-    bit_depth=6,
-    rgb_pins=[
-        board.MTX_R1,
-        board.MTX_G1,
-        board.MTX_B1,
-        board.MTX_R2,
-        board.MTX_G2,
-        board.MTX_B2,
-    ],
-    addr_pins=[board.MTX_ADDRA, board.MTX_ADDRB, board.MTX_ADDRC, board.MTX_ADDRD],
-    clock_pin=board.MTX_CLK,
-    latch_pin=board.MTX_LAT,
-    output_enable_pin=board.MTX_OE,
-)
+# matrix = rgbmatrix.RGBMatrix(
+#     width=32,
+#     height=32,
+#     bit_depth=6,
+#     rgb_pins=[
+#         board.MTX_R1,
+#         board.MTX_G1,
+#         board.MTX_B1,
+#         board.MTX_R2,
+#         board.MTX_G2,
+#         board.MTX_B2,
+#     ],
+#     addr_pins=[board.MTX_ADDRA, board.MTX_ADDRB, board.MTX_ADDRC, board.MTX_ADDRD],
+#     clock_pin=board.MTX_CLK,
+#     latch_pin=board.MTX_LAT,
+#     output_enable_pin=board.MTX_OE,
+# )
 
 display = framebufferio.FramebufferDisplay(matrix, auto_refresh=True)
 
